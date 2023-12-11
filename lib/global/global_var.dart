59ab70b3b87fc7d1b5fd312649efdd1b79309424
qminhminh <hqminh050503@gmail.com>
2023-12-11 21:27:44 +0700

    dỉever map position

--- lib/global/global_var.dart
+++ lib/global/global_var.dart
@@ -2,9 +2,9 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 String userName = "";
 
-String googleMapKey = "paste your key here";
+String googleMapKey = "AIzaSyDvSKI9ES_r3GzlR29FSwnaOZWxGLN93QQ";
 
 const CameraPosition googlePlexInitialPosition = CameraPosition(
-  target: LatLng(37.42796133580664, -122.085749655962),
+  target: LatLng(37.42796133580664, 122.085749655962),
   zoom: 14.4746,
 );
